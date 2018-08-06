6c1ed4d7381eb4551c3d9e8d58ea4194dfe56afe
Eddie Cho <cholihao@gmail.com>
2018-08-06 16:24:43 +0800

    first commit with quadruple and single large margin softmax loss in mnist

---
+++ visualize.py
@@ -0,0 +1,32 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def draw(path):
+    with open(path,'r') as f:
+        lines = f.readlines()
+
+    d = dict()
+
+    for line in lines:
+        sps = line.split()
+        label = int(sps[0])
+        if label not in d:
+            d[label] = []
+        pt = [float(sps[1]), float(sps[2])]
+        d[label].append(pt)
+    print len(d)
+    color = ['blue','green','red','cyan','magenta',
+        'yellow','black','white','gray','pink']
+    for key in d:
+        print str(key) + "\t" + str(len(d[key]))
+        val = np.array(d[key])
+        x = val[:,0]
+        y = val[:,1]
+        plt.scatter(x,y, label=key, c = color[key])
+
+    plt.legend()
+
+draw('largemargin_loss_feat.log')
+#draw('softmax_loss_feat.log')
+
+plt.show()
